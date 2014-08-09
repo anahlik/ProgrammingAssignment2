9a7811660ccbb9293c3385351b119bcad2571c6f
anahlik <anahlik+git@gmail.com>
2014-08-09 01:00:12 -0500

    Created cachematrix.r
    For homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I adapted the functions given in the example to work with the inverse of a matrix instead of a mean
 
-## Write a short comment describing this function
+## This sets up aa vector with the functions for set, get, setinverse, and getinverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## This does the solving for the inverse of the matrix or pulls for the cache if it has already been solved
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
 }
